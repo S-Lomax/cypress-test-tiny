91afb7ff32310d512daa7f33817e1c21b116fdaa
Stephen Lomax <stephen.lomax@globalservs.com>
2023-10-31 14:20:13 +0000

    bug repoduction

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 // <reference types="cypress" />
 describe.only('Upload image', () => {
   it('Confirm users are able to upload an image', () => {
-    cy.visit('');
+    cy.visit('/');
     cy.get('[data-cy="upload-input"]').first().selectFile('cypress/fixtures/QA_Automation_Permanent_Links_Slide_QR-CODE.png', { force: true })
 
     cy.get('[data-cy="slide-background"]')
